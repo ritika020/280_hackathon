2bf6ee12f6aacf5e120fc279d7948771bb5b4b3c
AdithyaSJSU <adithyathirumalai@sjsu.edu>
2025-03-30 13:43:51 -0700

    adding feats

--- agent-orchestrator/orchestrator.py
+++ agent-orchestrator/orchestrator.py
@@ -2,6 +2,8 @@ from agents.web_agent import handle_web_query
 from agents.clinical_agent import handle_clinical_query
 from agents.food_agent import handle_food_query
 from agents.data_agent import handle_data_query
+from db import db
+from datetime import datetime
 
 # Simple keyword-based classification (you can improve this later)
 def classify_query(query: str) -> str:
@@ -19,13 +21,30 @@ def classify_query(query: str) -> str:
         print("[Orchestrator] Routing to Web agent")
         return "web"
 
-async def handle_query(query: str) -> str:
+async def handle_query(query: str,session_id: str) -> str:
     agent_type = classify_query(query)
     if agent_type == "data":
-        return await handle_data_query(query)
+        answer = await handle_data_query(query)
     elif agent_type == "clinical":
-        return await handle_clinical_query(query)
+        answer =  await handle_clinical_query(query)
     elif agent_type == "food_security":
-        return await handle_food_query(query)
+        answer = await handle_food_query(query)
     else:
-        return await handle_web_query(query)
+        answer = await handle_web_query(query)
+        answer=answer.content
+
+
+    await db.sessions.update_one(
+        {"session_id": session_id},
+        {
+            "$push": {
+                "messages": {
+                    "prompt": query,
+                    "answer": getattr(answer, "content", answer),
+                    "timestamp": datetime.utcnow()
+                }
+            }
+        }
+    )
+    ##print(answer)
+    return answer
